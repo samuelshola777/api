675e0c39a23a95d3356ce90944ca2f9917a7bddb
samuel_shoal <samuelshola14@gmail.com>
2023-05-13 07:41:16 +0100

    coming up

--- src/main/java/com/pokemonreview/api/security/SecurityConfig.java
+++ src/main/java/com/pokemonreview/api/security/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.pokemonreview.api.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSocketSecurity
 public class SecurityConfig {
+
+public SecurityFilterChain filterChain(HttpSecurity http){
+
+}
+
 }
